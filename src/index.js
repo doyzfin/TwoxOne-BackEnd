0fecc637c799442b19e89315d2abced1a5f9de8d
idoy <alfinramadhan97@gmail.com>
2021-05-17 13:25:41 +0700

    update root app fix

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ app.use(compression())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use('/backend1/api/v1', routerNavigation)
-app.use('/banckend1/api', express.static('src/uploads'))
+app.use('/backend1/api', express.static('src/uploads'))
 
 app.post('/movie', (req, res) => {
   console.log('Post Movie Work')
