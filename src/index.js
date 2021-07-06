73c808ed287bdb2e2de71f86c89a226cd926b7bc
idoy <alfinramadhan97@gmail.com>
2021-07-07 00:41:14 +0700

    some revisi

--- src/index.js
+++ src/index.js
@@ -19,12 +19,6 @@ app.use(bodyParser.json())
 app.use('/backend1/api/v1', routerNavigation)
 app.use('/backend1/api', express.static('src/uploads'))
 
-app.post('/movie', (req, res) => {
-  console.log('Post Movie Work')
-  console.log(req.body.movieName)
-  // res.status(404).send('Hello World')
-})
-
 app.listen(port, () => {
   console.log(`Express app is lisneting on port ${port} !`)
 })
